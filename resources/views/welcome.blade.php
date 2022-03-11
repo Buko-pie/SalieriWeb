3d9da75b28e455334d46e519b6daddb4c74c0806
Salvador Amba Jr <salvadorjr.amba@gmail.com>
2022-03-11 22:15:27 +0800

    site icon and title change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,9 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Laravel</title>
+  <title>jl-illust</title>
+
+  <link rel="icon" href="{{ url('images/favicon.png') }}">
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
